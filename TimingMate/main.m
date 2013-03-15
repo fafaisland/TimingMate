a713a611297b6176340d392eb3f277cf3b0284ea
easonfafa <mac@macmatoMacBook-Pro.local>
2013-03-15 15:13:26 -0400

    init project

---
+++ TimingMate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimingMate
+//
+//  Created by easonfafa on 3/15/13.
+//  Copyright (c) 2013 TimingMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
